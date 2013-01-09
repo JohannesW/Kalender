0fb9244c970e0035e6eb51dd643aa78e661ae128
cyBerta <richy@cyborgsociety.org>
2013-01-09 20:07:10 +0100

    bugfix time zone

--- app/models/event.rb
+++ app/models/event.rb
@@ -30,19 +30,19 @@ class Event < ActiveRecord::Base
      date_start = to_Date(date_start)
      date_end = to_Date(date_end)
 
-     @result_query = Event.where('user_id = ?', "#{uid}")
+     @result_query = Event.where('user_id = ?', "#{uid}").order('dtstart')
      if (date_start < date_end)
-     @result_query = @result_query&Event.where('dtstart > ? AND dtstart < ?', "#{date_start}", "#{date_end}")
+     @result_query = @result_query&Event.where('dtstart > ? AND dtstart < ?', "#{date_start}", "#{date_end}").order('dtstart')
      end
      if search
-       @result_query = @result_query&Event.where('summary LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%")
+       @result_query = @result_query&Event.where('summary LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%").order('dtstart')
      end
      
  
   end
   
   def self.this_day(uid)
-   where('dtstart BETWEEN ? AND ? AND user_id = ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day, "#{uid}")
+   where('dtstart BETWEEN ? AND ? AND user_id = ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day, "#{uid}").order('dtstart')
   end
   
   def self.this_week(date, uid)
@@ -53,18 +53,18 @@ class Event < ActiveRecord::Base
     end
     @date_start = date.clone.to_date.at_beginning_of_week
     puts @date_start
-    @date_start = @date_start.to_datetime.beginning_of_day
+    @date_start = @date_start.clone.to_datetime
     puts @date_start
     @date_end = date.clone.to_date.end_of_week
     puts @date_end
-    @date_end = @date_end.to_datetime.end_of_day
+    @date_end = @date_end.clone.to_datetime.end_of_day
     puts @date_end
-    Event.where('dtstart BETWEEN ? AND ? AND user_id = ?', "#{@date_start}", "#{@date_end}", "#{uid}")
+    Event.where('dtstart BETWEEN ? AND ? AND user_id = ?', "#{@date_start}", "#{@date_end}", "#{uid}").order('dtstart')
 
   end
 
   
- #{:key1=>"2013", :key2=>"1", :key3=>"1", :key4=>"20", :key5=>"10"}
+ #{:key1=>"2013", :key2=>"7", :key3=>"1", :key4=>"00", :key5=>"10"} => 1.7.2012
  #{:key1=>"2012", :key2=>"12", :key3=>"1", :key4=>"20", :key5=>"10"}
   def self.to_Date(date)
     @retString = date.values[2].clone<<"."<<date.values[1].clone<<"."<<date.values[0].clone<<" "<<date.values[3].clone<<":"<<date.values[4].clone
